94c563d2d143a89fc54c5e74a7ac34b1c4a8e9e5
Luriev Artem <lurievartem@gmail.com>
2016-02-06 00:38:39 +0200

    fix hmr for reducers for babel 6

--- shared/store/configureStore.js
+++ shared/store/configureStore.js
@@ -7,11 +7,12 @@ export default function configureStore(initialState){
     const store = createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk, promiseMiddleware));
+        applyMiddleware(thunk, promiseMiddleware)
+    );
 
     if(module.hot){
         module.hot.accept('../reducers', () =>{
-            const nextRootReducer = require('../reducers');
+            const nextRootReducer = require('../reducers').default;
             store.replaceReducer(nextRootReducer);
         });
     }
